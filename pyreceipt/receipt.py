0d567bb250744fe6392a4e89cce89a3abb0f42bc
Mira <szewczyk.mira@gmail.com>
2017-08-26 16:59:59 +0200

    Define Receipt class to read from file

---
+++ pyreceipt/receipt.py
@@ -0,0 +1,37 @@
+import os
+from PIL import Image
+import pytesseract
+import tempfile
+
+
+class Receipt:
+    MIN_WIDTH = 900
+
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self._validate_data()
+        self.image = self._process()
+
+    @property
+    def tmp_file_path(self):
+        return self.file_name + '_tmp.jpg'
+
+    def _validate_data(self):
+        if not os.path.exists(self.file_name):
+            raise ValueError('This file does not exists!')
+
+    def _process(self):
+        # File should have at least 900px width to be read properly
+        img = Image.open(self.file_name)
+        width_percent = (self.MIN_WIDTH/float(img.size[0]))
+        height = int(float(img.size[1])*float(width_percent))
+        img = img.resize((self.MIN_WIDTH, height), Image.ANTIALIAS)
+        img.save(self.tmp_file_path)
+        return Image.open(self.tmp_file_path)
+
+
+    def read(self):
+        return pytesseract.image_to_string(self.image)
+
+    def delete_tmp_file(self):
+        os.remove(self.tmp_file_path)
