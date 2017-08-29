ca52651c8d7dd3c02e466f029e847c21fab59eae
Mira <szewczyk.mira@gmail.com>
2017-08-29 22:08:49 +0200

    Refactor code, set dpi when saving tmp file

--- pyreceipts/receipt.py
+++ pyreceipts/receipt.py
@@ -8,30 +8,31 @@ class Receipt:
     MIN_WIDTH = 900
 
     def __init__(self, file_name):
+        self._validate_image(file_name)
+
         self.file_name = file_name
-        self._validate_data()
-        self.image = self._process()
+        self.tmp_file_path = '%s_tmp.jpg' % file_name
+        self.original_file = Image.open(self.file_name)
 
-    @property
-    def tmp_file_path(self):
-        return self.file_name + '_tmp.jpg'
+        self._enlarge_image(self.original_file)
+        self.image = Image.open(self.tmp_file_path)
 
-    def _validate_data(self):
-        if not os.path.exists(self.file_name):
+    def _save_to_tmp(self, img):
+        img.save(self.tmp_file_path, dpi=(600, 600))
+        return img
+
+    def _validate_image(self, file_name):
+        if not os.path.exists(file_name):
             raise ValueError('This file does not exists!')
 
-    def _process(self):
-        # File should have at least 900px width to be read properly
-        img = Image.open(self.file_name)
-        if img.size[0] < self.MIN_WIDTH:
-            img = self._enlarge_image(img)
-        img.save(self.tmp_file_path)
-        return Image.open(self.tmp_file_path)
-
     def _enlarge_image(self, img):
-        width_percent = (self.MIN_WIDTH/float(img.size[0]))
-        height = int(float(img.size[1])*float(width_percent))
-        return img.resize((self.MIN_WIDTH, height), Image.ANTIALIAS)
+        # File should have at least 900px width to be read properly
+        if img.size[0] < self.MIN_WIDTH:
+            width_percent = (self.MIN_WIDTH/float(img.size[0]))
+            height = int(float(img.size[1])*float(width_percent))
+            img = img.resize((self.MIN_WIDTH, height), Image.ANTIALIAS)
+        self._save_to_tmp(img)
+        return img
 
     def read(self):
         return pytesseract.image_to_string(self.image)
